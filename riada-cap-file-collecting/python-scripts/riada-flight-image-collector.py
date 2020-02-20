256ed13537e5a23414cf26b78c001b2d5d53673e
Jacob Ellena <jacobellena@Jacobs-MacBook-Pro.local>
2020-02-20 18:56:36 -0500

    RIADA updates and readme edits

--- riada-cap-file-collecting/python-scripts/riada-flight-image-collector.py
+++ riada-cap-file-collecting/python-scripts/riada-flight-image-collector.py
@@ -16,14 +16,20 @@ from pathlib import Path
 # Begining timer
 t0 = time.time()
 
-# Requesting user to enter FEMA "event number"
+# Requesting user to enter FEMA site "event number"
 event_number = input("Enter Event Number:\n ")
+
+# Requesting user to enter FEMA site "flight number"
+flight_number = input("Enter Flight Number:\n ")
 
 # Requesting time to sleep between image grabs. Allows user to set larger in low bandwidth situations
 sleep_number = float(input("Enter time to wait between image grabs. \nConsider larger values for low bandwidth connections.\n"))
 
+# Creating directory variable
+directory = f'event_{event_number}_flight_{flight_number}'
+
 # Creating Directory on user's Desktop based on event number
-os.mkdir(os.path.abspath(Path.home()) + f'/Desktop/{event_number}/')
+os.mkdir(os.path.abspath(Path.home()) + f'/Desktop/{directory}/')
 
 # Assigning browser to be Firefox
 driver = webdriver.Firefox()
@@ -34,25 +40,19 @@ time.sleep(3)
 base_url = (f'http://fema-cap-imagery.s3-website-us-east-1.amazonaws.com/Images/{event_number}/')
 
 # Collecting page data
-driver.get(base_url)
+driver.get(base_url + flight_number + '/')
 time.sleep(5)
-
-# Pulling everything under the 'listing' element into a variable and converting to list
-flight_list = driver.find_element_by_id('listing').text.split()
-
-# Generating list individual flight events
-# Checking against substrings and removing '/' from each itme
-flight_list = [i.strip('/') for i in flight_list if ('/' in i) and ('..' not in i)]
 
 # Creating dataframe to be used as log
 scrape_log = pd.DataFrame(columns=('Status Code', 'Time of Grab', 'File Name'))
 
 # Function to grab image and write to flight specific directory
-def image_grab(image, flight, event_number):
+def image_grab(image, flight, event):
     # Setting scrape_log dataframe to be global so it can be added to
     global scrape_log
+    global directory
     # Setting image url based on directory parameters 
-    image_url = f'https://s3.amazonaws.com/fema-cap-imagery/Images/{event_number}/{flight}/{image}'
+    image_url = f'https://s3.amazonaws.com/fema-cap-imagery/Images/{event}/{flight}/{image}'
     # Grabbing local machine time for use in log
     grab_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     # Requesting image
@@ -62,7 +62,7 @@ def image_grab(image, flight, event_number):
     # Getting request status code
     status_code = image_grab_request.status_code
     # Generating file name for writing and log
-    file_name = f'{event_number}_{flight}_{image}'
+    file_name = f'{event}_{flight}_{image}'
     # Dataframe of current pull to append to scraper_log
     temp_scrape_log = pd.DataFrame(
         {
@@ -73,33 +73,28 @@ def image_grab(image, flight, event_number):
     )
     # Updating and writing scraper_log
     scrape_log = (scrape_log.append(temp_scrape_log)).reset_index(drop=True)
-    scrape_log.to_csv((os.path.abspath(Path.home()) + (f'/Desktop/{event_number}/scrape-log.csv')))
+    scrape_log.to_csv((os.path.abspath(Path.home()) + (f'/Desktop/{directory}/scrape-log.csv')))
     # Writing image to disk
-    open(os.path.expanduser(f'~/Desktop/{event_number}/{flight}/{file_name}'), 'wb').write(image_grab_request.content)
+    open(os.path.expanduser(f'~/Desktop/{directory}/{file_name}'), 'wb').write(image_grab_request.content)
 
-# Iterate through list of flights
-for flight in flight_list:
-    # Making directory to store images
-    os.mkdir(os.path.abspath(Path.home()) + f'/Desktop/{event_number}/{flight}')
-    # Loading flight event page into webdriver
-    driver.get(base_url + flight + '/')
-    # Pausing script to allow page to fully load
-    time.sleep(10)
-    # Calling list of all image file names
-    temp_image_file_list = driver.find_element_by_id('listing').text.split()
-    temp_image_file_list = [i for i in temp_image_file_list if '.jpg' in i]
-    # Running through each file with a check for an error and to retry if server times out on image request
-    for image in temp_image_file_list:
-        try:
-            image_grab(image, flight, event_number)
-        except:
-            print('Failure Occured')
-            # Giving time for server to potentially fix whatever blip occured and trying request again
-            time.sleep(30)
-            print('Trying again after sleep')
-            image_grab(image, flight, event_number)
-            print('Trying again')
 
+# Calling list of all image file names
+temp_image_file_list = driver.find_element_by_id('listing').text.split()
+temp_image_file_list = [i for i in temp_image_file_list if '.jpg' in i]
+
+# Running through each file with a check for an error and to retry if server times out on image request
+for image in temp_image_file_list:
+    try:
+        image_grab(image, flight_number, event_number)
+    except:
+        print('Failure Occured')
+        # Giving time for server to potentially fix whatever blip occured and trying request again
+        time.sleep(30)
+        print('Trying again after sleep')
+        image_grab(image, flight_number, event_number)
+        print('Trying again')
+
+            
 # Closing Firefox
 driver.close()
 
